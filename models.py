0097f85fc5e355db314561d257cdf00c1d93bfff
Zach Corum <57773927+HillviewCap@users.noreply.github.com>
2024-07-04 16:18:08 -0400

    Added password hashing functionality to the User model.

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 from pydantic import BaseModel
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from werkzeug.security import check_password_hash
 
 db = SQLAlchemy()
 
@@ -10,6 +11,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class SearchParams(BaseModel):
     query: str
