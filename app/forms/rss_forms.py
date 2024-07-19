309f1e34e13bb10abfa27ca3e94082ba027c2beb
HillviewCap <zcorum2012@gmail.com>
2024-07-19 13:46:31 -0400

    rss_forms page back on line

--- app/forms/rss_forms.py
+++ app/forms/rss_forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, FileField, TextAreaField
+from wtforms.validators import DataRequired, URL, Optional
 
 class AddRSSFeedForm(FlaskForm):
     url = StringField('RSS Feed URL', validators=[DataRequired(), URL()])
@@ -10,3 +10,11 @@ class AddRSSFeedForm(FlaskForm):
 class ImportCSVForm(FlaskForm):
     file = FileField('CSV File', validators=[DataRequired()])
     submit = SubmitField('Import')
+
+class EditRSSFeedForm(FlaskForm):
+    url = StringField('RSS Feed URL', validators=[DataRequired(), URL()])
+    title = StringField('Title', validators=[DataRequired()])
+    category = StringField('Category', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[Optional()])
+    last_build_date = StringField('Last Build Date', validators=[Optional()])
+    submit = SubmitField('Update Feed')
