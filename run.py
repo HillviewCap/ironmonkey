3a04706b8824951b697e40d324b4930e461b320f
HillviewCap <zcorum2012@gmail.com>
2024-07-19 01:13:55 -0400

    Added a new entry point to run the Flask application in debug mode.

--- run.py
+++ run.py
@@ -6,3 +6,9 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
