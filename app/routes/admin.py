ffbd7b93b1f5740f355e7c1af2fb5c4cae0eac45
HillviewCap <zcorum2012@gmail.com>
2024-07-19 13:56:36 -0400

    Added admin route to update awesome threat intelligence from CSV.

---
+++ app/routes/admin.py
@@ -0,0 +1,7 @@
+@admin.route('/update_awesome_threat_intel', methods=['POST'])
+@login_required
+@admin_required
+def update_awesome_threat_intel():
+    AwesomeThreatIntelService.update_from_csv()
+    flash('Awesome Threat Intel Blogs have been updated.', 'success')
+    return redirect(url_for('admin.rss_feeds'))
